dce1311c5fbebe4d432c45cc52a8887b05888545
dimanvaz <dimanvaz04@gmail.com>
2022-03-08 23:03:50 +0300

    deployed server to Heroku

--- src/components/Ierarchical.vue
+++ src/components/Ierarchical.vue
@@ -3,11 +3,18 @@
     <h1>{{ msg }}</h1>
     <table class="main_table">
       <tr>
-        <th v-for="field in Object.keys(citizens[0])" :key="field">{{field}}</th>
+<!--        <th v-for="key in Object.keys(citizens[0])" :key="key" >{{key}}</th>-->
+        <th>Город</th>
+        <th>Район</th>
+        <th>Улица</th>
+        <th>Имя</th>
       </tr>
       <tr v-for="citizen in citizens" :key="citizen">
-        <td v-for="(value,index) in citizen" :key="value" :class="index" :data-city="cityDetector(citizen)" @mouseover="onHover">{{value}}</td>
-
+<!--        <td v-for="(value,index) in citizen" :key="value" :class="index" :data-city="cityDetector(citizen)" @mouseover="onHover">{{value}}</td>-->
+        <td :class="citizen?.city">{{citizen?.city}}</td>
+        <td :class="citizen?.district">{{citizen?.district}}</td>
+        <td :class="citizen?.street">{{citizen?.street}}</td>
+        <td :data-city="cityDetector(citizen)" @mouseover="onHover" class="name">{{citizen?.name}}</td>
       </tr>
       <div  id="tooltip"
             :style="{ top: `${vertPos}px`, left: `${horizPos}px` }"
@@ -63,21 +70,41 @@ export default {
   created(){
     this.fetcher();
   },
+  updated() {
+    this.paint()
+  },
   methods: {
     async fetcher(){//---запрос к серверу для получения городов и горожан из БД------
       try{
-        let citiesAndCitizens = await fetch('http://127.0.0.1:3003/getCiCi')
+        let citiesAndCitizens = await fetch(
+            //'http://127.0.0.1:3003/getCiCi' // - локальный адрес при запуске с локалхостА
+            'https://steadyserv2.herokuapp.com/getCiCi' // - удалённый сервер на Хероку
+        )
             .then(response => response.json())
         this.cities = citiesAndCitizens.cities;
         this.citizens = citiesAndCitizens.citizens;
-        //console.log(this.citizens)
+
+        this.sortAndChange();
       } catch(e){ console.error(`Error from Ierarchical.vue/created(): `,e)}
+    },
+    //функция сортировки
+    sortAndChange(){
+      this.citizens.sort(function (citiz1, citiz2) {
+        for (let i = 3; i < Object.values(citiz1).length; i++) {
+          let res = Object.values(citiz1)[i].localeCompare(Object.values(citiz2)[i]);
+          if(res !==0) {
+            return res;
+          }
+        }
+      })
+      console.log(this.citizens)
     },
     //определем город по совпадению id города и city_id жителя
     cityDetector(citizen){
       let match = this.cities.find(city => {
         return city.id === citizen.city_id;
       });
+      //console.log([match.name,match.data])
       return [match.name,match.data]
     },
     //при наведении мыши, если у элемента класс = "name", покажем тултип
@@ -95,6 +122,24 @@ export default {
     //скрыть тултип
     clear(){
       this.show = false;
+    },
+    //выделить цветом ячейки
+    paint(){
+      let p = {};
+      document.querySelectorAll('td').forEach(domel => {
+        if(domel.classList.value !== 'name'){
+          p[domel.classList.value] = {}
+        }
+      })
+      Object.keys(p).forEach(key => {
+        let els = document.getElementsByClassName(key);
+        console.log(els)
+        let color = `${Math.random()*255},${100},${Math.random()*255}`
+        for (let node of els) {
+          node.style = `border:2px solid rgb(${color});`
+        }
+      })
+      console.log(p)
     }
   }
 }
@@ -131,6 +176,11 @@ a {
   padding: 5px;
   position: absolute;
   background-color: white;
+}
+
+td{
+  border:1px dashed blue;
+  padding: 5px;
 }
 
 </style>
